6db9b2defde2c1823b37b8c745e3104df1704e9b
Etienne <etienne.laramee@gmail.com>
2025-02-23 22:24:32 -0500

    Add state names object

--- Assignment 2 - Text search Boyer-Moore/TextSearch.java
+++ Assignment 2 - Text search Boyer-Moore/TextSearch.java
@@ -1,3 +1,11 @@
 public class TextSearch {
+    public void displayText() {
 
+    }
+
+    public static void main(String[] args) {
+        for (String state : States.getStates()) {
+            System.out.println("[" + state + "]");
+        }
+    }
 }
